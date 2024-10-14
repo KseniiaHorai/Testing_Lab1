90aac0768b90c81b12be92f8aec403e8ad565aee
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2024-10-14 13:13:20 +0300

    Add Successful Test

--- src/test/java/org/gameoflife/GameOfLifeTest.java
+++ src/test/java/org/gameoflife/GameOfLifeTest.java
@@ -6,9 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameOfLifeTest {
     @Test
-    void testCorrect() {
-        int sum = 5;
+    void testCorrect1() {
+        int sum = 4;
         assertEquals(2 + 2, sum);
+    }
+
+    @Test
+    void testCorrect2() {
+        int sum = 4;
+        assertEquals(2 * 2, sum);
     }
 
 }
