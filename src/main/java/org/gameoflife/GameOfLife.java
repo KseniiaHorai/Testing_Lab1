eb7935ada006a561341a14a9f469c6a64f6e5d10
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2024-10-14 23:48:38 +0300

    Add the readField method and tests for it

--- src/main/java/org/gameoflife/GameOfLife.java
+++ src/main/java/org/gameoflife/GameOfLife.java
@@ -6,8 +6,13 @@ import java.util.*;
 class GameOfLife {
 
     // Метод для зчитування поля з файлу
-    public static void readField() throws IOException {
-        //TODO
+    public static char[][] readField(BufferedReader br, int rows, int cols) throws IOException {
+        char[][] field = new char[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            String line = br.readLine();
+            field[i] = line.toCharArray();
+        }
+        return field;
     }
 
     // Метод для підрахунку сусідів клітини
@@ -36,6 +41,24 @@ class GameOfLife {
     }
 
     public static void main(String[] args) throws IOException {
-        //TODO
+        // Зчитування даних з файлу
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        int generations = Integer.parseInt(br.readLine());
+
+        System.out.println(generations);
+        System.out.println("generations");
+
+        String[] inputParams = br.readLine().split(" ");
+
+        int cols = Integer.parseInt(inputParams[0]);
+        int rows = Integer.parseInt(inputParams[1]);
+        System.out.println(Arrays.toString(inputParams));
+        System.out.println("inputParams");
+
+        char[][] field = readField(br, rows, cols);
+        System.out.println(Arrays.deepToString(field));
+        System.out.println("field");
+
+        br.close();
     }
 }
--- src/test/java/org/gameoflife/GameOfLifeTest.java
+++ src/test/java/org/gameoflife/GameOfLifeTest.java
@@ -2,13 +2,51 @@ package org.gameoflife;
 
 import org.junit.jupiter.api.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameOfLifeTest {
+
+    private static final String INPUT_FILE = "test_input.txt";
+
     @Test
-    void testCorrect() {
-        int sum = 4;
-        assertEquals(2 + 2, sum);
+    void testReadField() throws IOException {
+        String input = "3\n8 5\n........\n..x.....\n..x.....\n..x.....\n........";
+
+        Path path = Paths.get(INPUT_FILE);
+        Files.write(path, input.getBytes());
+
+        // Зчитування даних з файлу
+        BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE));
+
+        // Зчитування поколінь
+        int generations = Integer.parseInt(br.readLine());
+
+        // Зчитування розмірів поля
+        String[] inputParams = br.readLine().split(" ");
+        int cols = Integer.parseInt(inputParams[0]);
+        int rows = Integer.parseInt(inputParams[1]);
+
+        // Зчитування початкового поля
+        char[][] field = GameOfLife.readField(br, rows, cols);
+        assertEquals(3, generations);
+        assertEquals(8, cols);
+        assertEquals(5, rows);
+        assertArrayEquals(new char[][] {
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', 'x', '.', '.', '.', '.', '.'},
+                {'.', '.', 'x', '.', '.', '.', '.', '.'},
+                {'.', '.', 'x', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.'},
+        }, field);
+        br.close();
+        Files.deleteIfExists(path);
     }
 
 }
