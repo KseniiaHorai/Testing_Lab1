fe9e7d9122c8e748657667cf2f74fb078722f947
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2024-10-14 12:14:33 +0300

    Add CI

--- src/test/java/org/gameoflife/GameOfLifeTest.java
+++ src/test/java/org/gameoflife/GameOfLifeTest.java
@@ -1,7 +1,14 @@
 package org.gameoflife;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameOfLifeTest {
+    @Test
+    void testCorrect() {
+        int sum = 4;
+        assertEquals(2 + 2, sum);
+    }
 
 }
